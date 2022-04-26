b00196feec1a1fec99a546009d12dd016e7bdca6
Ali Kifah <olver422@yahoo.com>
2022-04-26 14:51:43 +0200

    Update Info.cs

--- Info.cs
+++ Info.cs
@@ -1,3 +1,13 @@
+// ============================================================================
+//    Author: Al-Khafaji, Ali Kifah
+//    Date:   20.4.2022
+//    Description: fully serializable abstract C# class that implements Reflection to convert field members to and from binary data.
+//    This class is meant to be used as a containing medium to hold and transfer Information on disk or over network.
+//    The classes that inherit from this class can be converted to byte array and transferred over network or saved to disk as a binary file.
+//    Loading of the original data from file or from byte array is also possible.
+// ============================================================================
+using System;
+
 ﻿using System;
 using System.IO;
 using System.Reflection;
