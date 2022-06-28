3024adb5591c43df12780f2c47b218328a33c61a
Ali Kifah <olver422@yahoo.com>
2022-06-28 23:19:58 +0200

    Add files via upload

---
+++ InfoObjectLibrary/ExtensionMethods.cs
@@ -0,0 +1,196 @@
+﻿// ============================================================================
+//    Author: Al-Khafaji, Ali Kifah
+//    Date:   20.4.2022
+//    Description: Extension Methods for BinaryReader and BinaryWriter
+// ============================================================================
+
+using System;
+using System.IO;
+
+
+namespace ExtensionMethods
+{
+	public static class BinExtensions
+	{
+		//################# string array
+		public static void WriteStringArray(this BinaryWriter w, string[] arr)
+		{
+			w.Write(arr.Length); // save length
+			foreach (string s in arr)
+				w.Write(s);
+		}
+		public static string[] ReadStringArray(this BinaryReader r)
+		{
+			int n = r.ReadInt32(); // load length of the array
+			string[] d = new string[n];
+			for (int i = 0; i < n; i++)
+				d[i] = r.ReadString();
+			return d;
+		}
+		//################# int array	
+		public static void WriteIntArray(this BinaryWriter w, int[] arr)
+		{
+			w.Write(arr.Length); // save length
+			foreach (int s in arr)
+				w.Write(s);
+		}
+		public static int[] ReadIntArray(this BinaryReader r)
+		{
+			int n = r.ReadInt32();// load length of the array
+			int[] d = new int[n];
+			for (int i = 0; i < n; i++)
+				d[i] = r.ReadInt32();
+			return d;
+		}
+		//################## byte array	
+		public static void WriteByteArray(this BinaryWriter w, byte[] arr)
+		{
+			w.Write(arr.Length);
+			foreach (byte s in arr)
+				w.Write(s);
+		}
+		public static byte[] ReadByteArray(this BinaryReader r)
+		{
+			int n = r.ReadInt32();
+			byte[] d = new byte[n];
+			for (int i = 0; i < n; i++)
+				d[i] = r.ReadByte();
+			return d;
+		}
+		//#############################
+		public static void WriteArrayOfByteArray(this BinaryWriter w, byte[][] arr)
+		{
+			w.Write(arr.Length);
+			foreach (byte[] s in arr)
+				w.WriteByteArray(s);
+		}
+		public static byte[][] ReadArrayOfByteArray(this BinaryReader r)
+		{
+			List<byte[]> k = new List<byte[]>();
+			int n = r.ReadInt32();// read count of byte arrays
+			for (int i = 0; i < n; i++)
+				k.Add(r.ReadByteArray());
+			byte[][] d = k.ToArray();
+			return d;
+		}
+		//################## float array	
+		public static void WriteFloatArray(this BinaryWriter w, float[] arr)
+		{
+			w.Write(arr.Length);
+			foreach (float s in arr)
+				w.Write(s);
+		}
+		public static float[] ReadFloatArray(this BinaryReader r)
+		{
+			int n = r.ReadInt32();
+			float[] d = new float[n];
+			for (int i = 0; i < n; i++)
+				d[i] = r.ReadByte();
+			return d;
+		}
+
+//############################### Datetime
+		public static void WriteDateTime(this BinaryWriter w, DateTime d)
+		{
+			w.Write(d.Year);
+			w.Write(d.Month);
+			w.Write(d.Day);
+			w.Write(d.Hour);
+			w.Write(d.Minute);
+			w.Write(d.Second);
+			w.Write(d.Millisecond);
+		}
+		public static DateTime ReadDateTime(this BinaryReader r)
+		{
+			DateTime d = new DateTime(r.ReadInt32(), r.ReadInt32(), r.ReadInt32(), r.ReadInt32(), r.ReadInt32(), r.ReadInt32(), r.ReadInt32());
+			return d;
+		}
+		//############################### Dict
+
+		public static void WriteDict_str_str(this BinaryWriter w, Dictionary<string,string> dict)
+		{
+			w.Write(dict.Count);
+			foreach (KeyValuePair<string,string> kvp in dict)
+            {
+				w.Write(kvp.Key);
+				w.Write(kvp.Value);
+			}	
+		}
+		public static Dictionary<string, string> ReadDict_str_str(this BinaryReader r)
+		{
+			int n = r.ReadInt32();
+			Dictionary<string, string> dict = new Dictionary<string, string>();
+			for (int i = 0; i < n; i++)
+			{
+				dict.Add(r.ReadString(), r.ReadString());
+			}
+			return dict;
+		}
+
+		public static void WriteDict_int_str(this BinaryWriter w, Dictionary<int, string> dict)
+		{
+			w.Write(dict.Count);
+			foreach (KeyValuePair<int, string> kvp in dict)
+			{
+				w.Write(kvp.Key);
+				w.Write(kvp.Value);
+			}
+		}
+		public static Dictionary<int, string> ReadDict_int_str(this BinaryReader r)
+		{
+			int n = r.ReadInt32();
+			Dictionary<int, string> dict = new Dictionary<int, string>();
+			for (int i = 0; i < n; i++)
+			{
+				dict.Add(r.ReadInt32(), r.ReadString());
+			}
+			return dict;
+		}
+		//----------
+		public static void WriteDict_int_int(this BinaryWriter w, Dictionary<int, int> dict)
+		{
+			w.Write(dict.Count);
+			foreach (KeyValuePair<int, int> kvp in dict)
+			{
+				w.Write(kvp.Key);
+				w.Write(kvp.Value);
+			}
+		}
+		public static Dictionary<int, int> ReadDict_int_int(this BinaryReader r)
+		{
+			int n = r.ReadInt32();
+			Dictionary<int, int> dict = new Dictionary<int, int>();
+			for (int i = 0; i < n; i++)
+			{
+				dict.Add(r.ReadInt32(), r.ReadInt32());
+			}
+			return dict;
+		}
+
+		//----------
+		public static void WriteDict_str_int(this BinaryWriter w, Dictionary<string, int> dict)
+		{
+			w.Write(dict.Count);
+			foreach (KeyValuePair<string, int> kvp in dict)
+			{
+				w.Write(kvp.Key);
+				w.Write(kvp.Value);
+			}
+		}
+		public static Dictionary<string, int> ReadDict_str_int(this BinaryReader r)
+		{
+			int n = r.ReadInt32();
+			Dictionary<string, int> dict = new Dictionary<string, int>();
+			for (int i = 0; i < n; i++)
+			{
+				dict.Add(r.ReadString(), r.ReadInt32());
+			}
+			return dict;
+		}
+
+
+
+
+
+	}
+}
---
+++ InfoObjectLibrary/ReflectionProvider.cs
@@ -0,0 +1,284 @@
+﻿
+
+using System.Reflection;
+using ExtensionMethods;
+
+
+public static class ReflectionProvider
+    {
+        public static PropertyInfo[] GetProperties(object infoclass)
+        {
+            PropertyInfo[] properties = infoclass.GetType().GetProperties();
+            return properties;
+        }
+
+        public static string GetPropertyName<T>(PropertyInfo p)
+        {
+            return p.Name;
+        }
+        public static object GetPropertyValue(PropertyInfo p , object infoclass) 
+        {
+            return p.GetValue(infoclass, null);
+        }
+    public static Type GetPropertyType(PropertyInfo p)
+    {
+
+        return p.PropertyType;
+
+    }
+
+    internal static PropertyInfo[] GetProperties<T>(object info)
+    {
+        PropertyInfo[] properties = info.GetType().GetProperties();
+        return properties;
+    }
+
+    //#################  list parsing code ########################################################
+    public static bool isList(PropertyInfo prop)
+    {
+        Type type = prop.PropertyType;
+        if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(List<>))
+            return true;
+        return false;
+    }
+
+
+    public static Type getListType(PropertyInfo prop)
+    {
+        Type type = prop.PropertyType;
+        if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(List<>))
+        {
+            return type.GetGenericArguments()[0]; // use this...
+        }
+        return null;
+    }
+
+
+    public static void WriteList(BinaryWriter w, PropertyInfo prop, object list)
+    {
+        Type t = getListType(prop);
+        if (t == typeof(string))
+        {
+            if (((List<string>)list) != null)
+            {
+                w.WriteStringArray(((List<string>)list).ToArray());
+            }
+            else
+            {
+                List<string> strings = new List<string>();
+                w.WriteStringArray(strings.ToArray());
+            }
+        }
+        else if (t == typeof(int))
+        {
+            if (((List<int>)list) != null)
+            {
+                w.WriteIntArray(((List<int>)list).ToArray());
+            }
+            else
+            {
+                List<int> ints = new List<int>();
+                w.WriteIntArray(ints.ToArray());
+            }
+        }
+        else if (t == typeof(float))
+        {
+            if (((List<float>)list) != null)
+            {
+                w.WriteFloatArray(((List<float>)list).ToArray());
+            }
+            else
+            {
+                List<float> floats = new List<float>();
+                w.WriteFloatArray(floats.ToArray());
+            }
+        }
+        else if (t == typeof(byte))
+        {
+            if (((List<byte>)list) != null)
+            {
+                w.WriteByteArray(((List<byte>)list).ToArray());
+            }
+            else
+            {
+                List<byte> bytes = new List<byte>();
+                w.WriteByteArray(bytes.ToArray());
+            }
+        }
+        else if (t.IsSubclassOf(typeof(Info)))
+        {
+
+        }
+
+        Console.WriteLine("----- list type:" + t);
+
+    }
+
+
+    public static void ReadList(BinaryReader r, object instance, PropertyInfo prop)//, object list)
+    {
+        Type t = getListType(prop);
+        if (t == typeof(string))
+        {
+            List<string> lst = new List<string>();
+            lst.AddRange(r.ReadStringArray());
+            prop.SetValue(instance, lst);
+        }
+        else if (t == typeof(int))
+        {
+            List<int> lst = new List<int>();
+            lst.AddRange(r.ReadIntArray());
+            prop.SetValue(instance, lst);
+        }
+        else if (t == typeof(float))
+        {
+            List<float> lst = new List<float>();
+            lst.AddRange(r.ReadFloatArray());
+            prop.SetValue(instance, lst);
+        }
+        else if (t == typeof(byte))
+        {
+            List<byte> lst = new List<byte>();
+            lst.AddRange(r.ReadByteArray());
+            prop.SetValue(instance, lst);
+        }
+
+    }
+
+    //############# dict parsing code ##########################################################
+    public static bool isDict(PropertyInfo prop)
+    {
+        Type type = prop.PropertyType;
+        if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Dictionary<,>))
+            return true;
+        return false;
+    }
+
+    public static Type getDictKeyType(PropertyInfo prop)
+    {
+        Type type = prop.PropertyType;
+        if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Dictionary<,>))
+        {
+            return type.GetGenericArguments()[0];
+        }
+        return null;
+    }
+    public static Type getDictValueType(PropertyInfo prop)
+    {
+        Type type = prop.PropertyType;
+        if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Dictionary<,>))
+        {
+            return type.GetGenericArguments()[1];
+        }
+        return null;
+    }
+
+    public static void WriteDict(BinaryWriter w, PropertyInfo prop, object dict)
+    {
+        Type keyType = getDictKeyType(prop);
+        Type valueType = getDictValueType(prop);
+        if (keyType == typeof(string) && valueType == typeof(string))
+        {
+            if (((Dictionary<string, string>)dict) != null)
+            {
+                w.WriteDict_str_str((Dictionary<string, string>)dict);
+            }
+            else
+            {
+                Dictionary<string, string> d = new Dictionary<string, string>();
+                w.WriteDict_str_str(d);
+            }
+
+        }
+        else if (keyType == typeof(int) && valueType == typeof(string))
+        {
+            if (((Dictionary<int, string>)dict) != null)
+            {
+                w.WriteDict_int_str((Dictionary<int, string>)dict);
+            }
+            else
+            {
+                Dictionary<int, string> d = new Dictionary<int, string>();
+                w.WriteDict_int_str(d);
+            }
+        }
+        else if (keyType == typeof(int) && valueType == typeof(int))
+        {
+            if (((Dictionary<int, int>)dict) != null)
+            {
+                w.WriteDict_int_int((Dictionary<int, int>)dict);
+            }
+            else
+            {
+                Dictionary<int, int> d = new Dictionary<int, int>();
+                w.WriteDict_int_int(d);
+            }
+        }
+
+        else if (keyType == typeof(string) && valueType == typeof(int))
+        {
+            if (((Dictionary<string, int>)dict) != null)
+            {
+                w.WriteDict_str_int((Dictionary<string, int>)dict);
+            }
+            else
+            {
+                Dictionary<string, int> d = new Dictionary<string, int>();
+                w.WriteDict_str_int(d);
+            }
+        }
+
+
+
+      
+
+    }
+
+
+
+    public static void ReadDict(BinaryReader r, object instance, PropertyInfo prop)
+    {
+        Type keyType = getDictKeyType(prop);
+        Type valueType = getDictValueType(prop);
+        if (keyType == typeof(string) && valueType == typeof(string))
+        {
+            Dictionary<string, string> d = r.ReadDict_str_str();
+            prop.SetValue(instance, d);
+        }
+        else if (keyType == typeof(int) && valueType == typeof(string))
+        {
+            Dictionary<int, string> d = r.ReadDict_int_str();
+            prop.SetValue(instance, d);
+        }
+        else if (keyType == typeof(int) && valueType == typeof(int))
+        {
+            Dictionary<int, int> d = r.ReadDict_int_int();
+            prop.SetValue(instance, d);
+        }
+
+        else if (keyType == typeof(string) && valueType == typeof(int))
+        {
+            Dictionary<string, int> d = r.ReadDict_str_int();
+            prop.SetValue(instance, d);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
